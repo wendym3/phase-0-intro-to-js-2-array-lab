0913e2b8ea18347a7cccbe07766371ac533d72ad
wendymasese <wendy.masese@student.moringaschool.com>
2024-04-03 00:20:33 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function catName() {
+    return cats
+    
+}
+cats();
+
+function destructivelyAppendCat(Ralph) {
+    cats.push("Ralph");
+  }
+
+function destructivelyPrependCat (Bob) {
+    cats.unshift("Bob"); 
+}
+
+function destructivelyRemoveLastCat (Garfield) {
+    cats.pop("Garfield");
+}
+
+function destructivelyRemoveFirstCat (Milo)  {
+    cats.shift("Milo");
+}
+
+function appendCat (Broom) {
+    return [...cats,Broom];
+   
+}
+
+function prependCat (Arnold) {
+    return [Arnold, ...cats];
+}
+
+function removeLastCat () {
+   return cats.slice(0, -1);
+
+}
+
+function removeFirstCat () {
+    return cats.slice (1);
+}
